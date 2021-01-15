e969935a88aaa1b3f59d49f01ff00398cad5eab1
Sophie <32652902+SleepyheadSophie@users.noreply.github.com>
2021-01-15 20:13:42 +0300

    Лекция 2.7
    Выделяем специализированные классы-помощники

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -7,8 +7,8 @@ public class ContactCreationTests extends TestBase {
 
     @Test
     public void testContactCreationTests() {
-        app.gotoContactCreationPage();
-        app.fillContactCreationForm(new ContactData("Alex", "Vladimirovich", "Maloenko",
+        app.getNavigationHelper().gotoContactCreationPage();
+        app.getcontactHelper().fillContactCreationForm(new ContactData("Alex", "Vladimirovich", "Maloenko",
                 "sleepyhead", "title1", "company1", "Moscow, Russia",
                 "111", "89160001122", "232", "123",
                 "maloenko@mail.ru", "mail1@mail.ru", "mail2@mai.ru", "ttt.com",
@@ -16,8 +16,8 @@ public class ContactCreationTests extends TestBase {
                 "17", "October", "2000",
                 "test1",
                 "123", "123", "123"));
-        app.submitContactCreation();
-        app.gotoHomePage();
+        app.getcontactHelper().submitContactCreation();
+        app.getNavigationHelper().gotoHomePage();
     }
 
 }
