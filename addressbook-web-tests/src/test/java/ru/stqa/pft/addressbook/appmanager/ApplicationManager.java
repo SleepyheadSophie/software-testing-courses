67941c402c678bd980c30b38138b089345ff709d
Sophie <32652902+SleepyheadSophie@users.noreply.github.com>
2021-01-15 19:00:25 +0300

    Лекция 2.6
    Заменяем наследование на делегирование и создаём двухуровневую архитектуру

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,4 +1,4 @@
-package ru.stqa.pft.addressbook;
+package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
@@ -6,23 +6,22 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.concurrent.TimeUnit;
 
-public class TestBase {
+public class ApplicationManager {
     WebDriver wd;
 
-    @BeforeMethod(alwaysRun = true)
-    public void setUp() {
+    public void init() {
         wd = new ChromeDriver();
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost/addressbook/");
         login("admin", "secret");
     }
 
-    private void login(String username, String password) {
+    public void login(String username, String password) {
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
         wd.findElement(By.name("user")).sendKeys(username);
@@ -32,15 +31,15 @@ public class TestBase {
         wd.findElement(By.xpath("//input[@value='Login']")).click();
     }
 
-    private void logout() {
+    public void logout() {
         wd.findElement(By.linkText("Logout")).click();
     }
 
-    protected void submitGroupCreation() {
+    public void submitGroupCreation() {
         wd.findElement(By.name("submit")).click();
     }
 
-    protected void fillGroupForm(GroupData groupData) {
+    public void fillGroupForm(GroupData groupData) {
         wd.findElement(By.name("group_name")).click();
         wd.findElement(By.name("group_name")).clear();
         wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
@@ -52,21 +51,20 @@ public class TestBase {
         wd.findElement(By.xpath("//textarea[@name='group_footer']")).sendKeys(groupData.getFooter());
     }
 
-    protected void initGroupCreation() {
+    public void initGroupCreation() {
         wd.findElement(By.name("new")).click();
     }
 
-    protected void gotoGroupPage() {
+    public void gotoGroupPage() {
         wd.findElement(By.linkText("groups")).click();
     }
 
-    @AfterMethod(alwaysRun = true)
-    public void tearDown() {
+    public void stop() {
         logout();
         wd.quit();
     }
 
-    private boolean isElementPresent(By by) {
+    public boolean isElementPresent(By by) {
         try {
             wd.findElement(by);
             return true;
@@ -75,7 +73,7 @@ public class TestBase {
         }
     }
 
-    private boolean isAlertPresent() {
+    public boolean isAlertPresent() {
         try {
             wd.switchTo().alert();
             return true;
@@ -84,19 +82,19 @@ public class TestBase {
         }
     }
 
-    protected void deleteSelectedGroups() {
+    public void deleteSelectedGroups() {
         wd.findElement(By.name("delete")).click();
     }
 
-    protected void selectGroup() {
+    public void selectGroup() {
         wd.findElement(By.name("selected[]")).click();
     }
 
-    protected void gotoContactCreationPage() {
+    public void gotoContactCreationPage() {
         wd.findElement(By.linkText("add new")).click();
     }
 
-    protected void fillContactCreationForm(ContactData contactData) {
+    public void fillContactCreationForm(ContactData contactData) {
         wd.findElement(By.name("firstname")).click();
         wd.findElement(By.name("firstname")).clear();
         wd.findElement(By.name("firstname")).sendKeys(contactData.getFirstname());
@@ -174,11 +172,11 @@ public class TestBase {
         wd.findElement(By.name("notes")).sendKeys(contactData.getNotes());
     }
 
-    protected void submitContactCreation() {
+    public void submitContactCreation() {
         wd.findElement(By.xpath("(//input[@name='submit'])")).click();
     }
 
-    protected void gotoHomePage() {
+    public void gotoHomePage() {
         wd.findElement(By.linkText("home page")).click();
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,13 +1,14 @@
-package ru.stqa.pft.addressbook;
+package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.*;
+import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactCreationTests extends TestBase {
 
     @Test
     public void testContactCreationTests() {
-        gotoContactCreationPage();
-        fillContactCreationForm(new ContactData("Alex", "Vladimirovich", "Maloenko",
+        app.gotoContactCreationPage();
+        app.fillContactCreationForm(new ContactData("Alex", "Vladimirovich", "Maloenko",
                 "sleepyhead", "title1", "company1", "Moscow, Russia",
                 "111", "89160001122", "232", "123",
                 "maloenko@mail.ru", "mail1@mail.ru", "mail2@mai.ru", "ttt.com",
@@ -15,8 +16,8 @@ public class ContactCreationTests extends TestBase {
                 "17", "October", "2000",
                 "test1",
                 "123", "123", "123"));
-        submitContactCreation();
-        gotoHomePage();
+        app.submitContactCreation();
+        app.gotoHomePage();
     }
 
 }
